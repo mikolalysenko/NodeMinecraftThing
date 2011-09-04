d63b91a98517ac0f2c9114a1efc453fc9a41ac72
Mikola <mikola@STUPIDHOSTNAME.(none)>
2011-09-04 03:42:03 -0500

    Added an error page for when webgl support fails

--- test_chrome.sh
+++ test_chrome.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-google-chrome http://localhost:8000/
+google-chrome --ignore-gpu-blacklist http://localhost:8000/
--- www/app.js
+++ www/app.js
@@ -61,7 +61,8 @@ var DefaultState = {
 //Called when the 
 var NoWebGLState = {
   init : function() {
-    alert("Your browser does not support WebGL :(");
+    document.getElementById('noGLPane').style.display = 'block';
+    document.getElementById('videoItem').innerHTML = '<iframe width="560" height="345" src="http://www.youtube.com/embed/bV8Yus_atoE" frameborder="0" allowfullscreen></iframe>';
   },
   
   shutdown : function() {
