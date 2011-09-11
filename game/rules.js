f0f6220bcbe737c1e5d758c1cbf4321241019558
Mikola <mikola@STUPIDHOSTNAME.(none)>
2011-09-11 02:53:36 -0500

    Need to figure out how to incorporate game logic into engine

---
+++ game/rules.js
@@ -0,0 +1,25 @@
+//Rules for the game
+var Rules;
+if(exports === undefined) {
+
+
+}
+else {
+  Rules = exports;
+}
+
+
+//Rules for thawing an entity
+Rules.thaw = function(entity) {
+};
+
+//Create the game world from scratch
+Rules.initializeWorld = function() {
+};
+
+//Locates initial position for a player
+Rules.createPlayerEntity = function() {
+};
+
+
+
