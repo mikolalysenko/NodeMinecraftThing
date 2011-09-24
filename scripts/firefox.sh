b1d41bc370ed8e3e1d3312d64d1233966a6beecd
Mikola <mikola@STUPIDHOSTNAME.(none)>
2011-09-24 00:14:15 -0500

    Updated readme

--- scripts/firefox.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-scripts/start.sh
-sleep 2
-firefox http://localhost:8000/
--- scripts/stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-pkill -9 node
-passwd=cat data/keyfile
-echo 'use admin
-db.auth("admin", $passwd);
-db.shutdownServer();' | mongo/mongo 127.0.0.1
-
