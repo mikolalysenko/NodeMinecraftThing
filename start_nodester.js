b713dea1cc0a3600b1f9f28f9072289128a23c66
Mikola <mikola@STUPIDHOSTNAME.(none)>
2011-09-28 00:06:39 -0500

    what now...

--- start_nodester.js
+++ start_nodester.js
@@ -25,7 +25,7 @@ var settings = {
   RESET       : true,
   
   //If this flag is set, don't compress the client
-  debug       : false,
+  debug       : true,
 };
 
 //Parse out arguments from commandline
